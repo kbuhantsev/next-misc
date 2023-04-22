bda02c141f965d18a19b404907816e7ccdd76bb4
Konstantyn <k.buhantsev@gmail.com>
2023-04-22 17:20:28 +0300

    dfsf

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
 import "@/styles/layout.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   if (Component.getLayout) {
@@ -10,6 +11,10 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>Home page</title>
+        <meta name="description" content="description of home page" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import Footer from "@/components/Footer";
+import Head from "next/head";
 import React from "react";
 
 const About = () => {
@@ -10,6 +11,10 @@ export default About;
 About.getLayout = function PageLayout(page) {
   return (
     <>
+      <Head>
+        <title>About page</title>
+        <meta name="description" content="description of about page" />
+      </Head>
       {page}
       <Footer />
     </>
